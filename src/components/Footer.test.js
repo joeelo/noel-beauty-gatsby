ddc59e36e7f0a30e5e78ec35ad823c5a8007a35c
Joseph <josephclorenzo@gmail.com>
2019-10-11 11:38:58 -0700

    jest installation and enzyme setup complete

---
+++ src/components/Footer.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Footer from "./Footer";
+import { shallow } from "enzyme";
+
+test("renders", () => {
+    shallow(<Footer/>);
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 // import Image from "../components/image";
 import SEO from "../components/seo";
 
-const imageSrc = "https://images.squarespace-cdn.com/content/v1/55b04019e4b08745c52f0b8d/1514923260447-VX5P8HVUHQW6ZPJ5SD0A/ke17ZwdGBToddI8pDm48kCgnL50anMCU6VaIstMxCzMUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2KJSnoLuGGsu3PSR8Wyu_Kkd-ZcdeJ_PurDofJWjrKkSbhuOb0MAcXaQ6whlEkpec/model.jpg";
+const imageSrc = "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80";
 
 const IndexPage = () => (
   <Layout>
