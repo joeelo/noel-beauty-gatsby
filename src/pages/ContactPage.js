bab1e5cdeb883cd8f72b9d4fdc60d36797d624f8
Joseph <josephclorenzo@gmail.com>
2019-10-17 14:59:22 -0700

    new code styling and centered contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -24,20 +24,35 @@ class ContactPage extends Component {
         return (
             <Layout>
                 <PageContainer>
-    
-                    <ContactSpan>  Contact Joanne Noel for Beauty tips and advice, or if you want to be featured in upcoming animations/videos write a quick message!   
-                    </ContactSpan>
-    
                     <ContactForm onSubmit={this.handleSubmit}>
-                        <Input type="text" name="name" onChange={this.handleChange} placeholder="name" value={this.state.name}/>
-                        <Input type="text" name="email" onChange={this.handleChange} placeholder="email" value={this.state.email}/>
-                        <TextArea name="content" placeholder="write your message here" onChange={this.handleChange} value={this.state.content}/>
-                        <button> submit </button>
+                        <ContactSpan>  
+                            Contact Joanne Noel for Beauty tips and advice, or to be featured in upcoming animations/videos write a quick message!   
+                        </ContactSpan>
+                        <Input 
+                            type="text" 
+                            name="name" 
+                            onChange={this.handleChange} 
+                            placeholder="name" 
+                            value={this.state.name}
+                        />
+                        <Input 
+                            type="text" name="email" 
+                            onChange={this.handleChange} 
+                            placeholder="email" 
+                            value={this.state.email}
+                        />
+                        <TextArea 
+                            name="content" 
+                            placeholder="write your message here" 
+                            onChange={this.handleChange} 
+                            value={this.state.content}
+                        />
+                    <button> submit </button>
                     </ContactForm>
     
                     <LinkContainer>
                         <ContactSocialLink> <Anchor href="#" target="_blank"> FB </Anchor></ContactSocialLink>
-                        <ContactSocialLink> <Anchor href="https://www.linkedin.com/in/joanne-noel-3008166/" target="_blank"> LKD </Anchor></ContactSocialLink>
+                        <Anchor href="https://www.linkedin.com/in/joanne-noel-3008166/" target="_blank"><ContactSocialLink>  LKD </ContactSocialLink></Anchor>
                         <ContactSocialLink> <Anchor href="https://www.instagram.com/joannenoel/?hl=en" target="_blank"> INS </Anchor></ContactSocialLink>
                     </LinkContainer>
                 </PageContainer>
@@ -50,24 +65,30 @@ class ContactPage extends Component {
 export default ContactPage;
 
 const PageContainer = styled.div`
-    width: 100vw;
+width: 100vw;
+display: flex;
+min-height: 400px;
+justify-content: space-around;
+flex-direction: column;
+`
+
+const ContactForm = styled.form`
     display: flex;
     flex-direction: column;
-    min-height: 400px;
-    justify-content: space-around;
+    align-items: flex-start;
+    margin-top: 4vh;
 `
 
 const ContactSpan = styled.span`
     font-family: Helvetica, sans-serfif;
     text-decoration: line-height: 1.25em;
-
-    @media (max-width: 460px) {
-        font-size: 12px;
-    }
+    grid-column-start: 2;
+    font-size: 18px;
 `
 
 const LinkContainer = styled.div`
     display: flex;
+    margin-top: 20px;
 `
 
 const ContactSocialLink = styled(SocialLink)`
@@ -84,20 +105,20 @@ const ContactAnchor = styled(Anchor)`
     
 `
 
-const ContactForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-`
 
 const Input = styled.input`
     border: 1px solid black;
-    margin: 5px 0; 
+    margin: 10px 0; 
     width: 40vw;
+    padding-left: 10px;
+    width: 75%;
 `
 
 const TextArea = styled.textarea`
     border: 1px solid black;
     width: 100%;
     margin: 5px 0;
+    height: 200px;
+    padding: 5px 10px;
 `
+
